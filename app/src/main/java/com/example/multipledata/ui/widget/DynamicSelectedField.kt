6c71b13b5bf7bcbd0d2c50b79b03d8bf2070274c
kaniadhea <kaniadhea7@gmail.com>
2024-11-21 10:39:35 +0700

    DropdownMenu yang digunakan untuk memberikan daftar pilihan tertentu kepada pengguna

--- app/src/main/java/com/example/multipledata/ui/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/multipledata/ui/widget/DynamicSelectedField.kt
@@ -1,9 +1,17 @@
 package com.example.multipledata.ui.widget
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.Lifecycle
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DynamicSelectTextField(
     selectedValue : String,
@@ -12,5 +20,16 @@ fun DynamicSelectTextField(
     onValueChangedEvent: (String) -> Unit,
     modifier: Modifier = Modifier
 ){
+    var expanded by remember { mutableStateOf(false) }
 
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = {expanded = !expanded},
+        modifier = modifier
+    ) {
+        OutlinedTextField(
+            readOnly = true,
+            value = selectedValue,
+        )
+    }
 }
