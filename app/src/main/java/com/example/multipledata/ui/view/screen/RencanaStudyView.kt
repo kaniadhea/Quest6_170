197b784c258ea92c75168d33932555e164797c23
kaniadhea <kaniadhea7@gmail.com>
2024-11-21 21:59:56 +0700

    Layout yang menampilkan komponen secara horizontal.

--- app/src/main/java/com/example/multipledata/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/multipledata/ui/view/screen/RencanaStudyView.kt
@@ -1,6 +1,24 @@
 package com.example.multipledata.ui.view.screen
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.multipledata.R
 import com.example.multipledata.model.Mahasiswa
 
 @Composable
@@ -9,5 +27,30 @@ fun RencanaStudyView(
     onSubmitButtonClicked : (MutableList<String>) ->Unit,
     onBackButtonClicked: () -> Unit
 ){
-
+    var chosenDropdown by remember{ mutableStateOf("")
+    }
+    var checked by remember { mutableStateOf(false) }
+    var pilihanKelas by remember { mutableStateOf("")
+    }
+    var  listData: MutableList<String> = mutableListOf(chosenDropdown, pilihanKelas)
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = colorResource(id = R.color.primary))
+    ){
+        Row (
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(40.dp),
+            verticalAlignment = Alignment.CenterVertically,
+        ){
+            Image(
+                painter = painterResource(
+                    id = R.drawable.umy
+                ),
+                contentDescription = ""
+            )
+            Spacer(modifier = Modifier.padding(start = 30.dp))
+        }
+    }
 }
