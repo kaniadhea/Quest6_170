8f63d8026cdc318d5f794e0b60bcbdccd561d641
kaniadhea <kaniadhea7@gmail.com>
2024-11-21 23:46:10 +0700

    Fungsi ini digunakan untuk memperbarui state kelas yang dipilih.

--- app/src/main/java/com/example/multipledata/ui/view/viewmodel/RencanaStudyViewModel.kt
+++ app/src/main/java/com/example/multipledata/ui/view/viewmodel/RencanaStudyViewModel.kt
@@ -1,10 +1,12 @@
 package com.example.multipledata.ui.view.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.room.util.copy
 import com.example.multipledata.model.RencanaStudi
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class RencanaStudyViewModel: ViewModel () {
     private val _krsState = MutableStateFlow(RencanaStudi())
@@ -12,6 +14,18 @@ class RencanaStudyViewModel: ViewModel () {
     val krsStateUi : StateFlow<RencanaStudi> = _krsState.asStateFlow()
 
     fun setMataKuliah(mkPilihan : String) {
+        _krsState.update { stateMK -> stateMK.copy(namaMK = mkPilihan) }
 
+    }
+
+    fun setKelas(kelasPilihan : String){
+        _krsState.update { stateKelas -> stateKelas.copy (kelas = kelasPilihan) }
+    }
+
+    fun saveDataKRS(ls: MutableList<String>) {
+        _krsState.update { status -> status.copy(
+            namaMK = ls[0],
+            kelas = ls [1]
+        ) }
     }
 }
