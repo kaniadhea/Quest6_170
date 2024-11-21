f1e030916acacfb7713b1a1614621701fc9d1000
kaniadhea <kaniadhea7@gmail.com>
2024-11-21 22:06:00 +0700

    Column yang digunakan untuk menyusun komponen secara vertikal

--- app/src/main/java/com/example/multipledata/ui/view/screen/RencanaStudyView.kt
+++ app/src/main/java/com/example/multipledata/ui/view/screen/RencanaStudyView.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -15,9 +16,12 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.multipledata.R
 import com.example.multipledata.model.Mahasiswa
 
@@ -51,6 +55,17 @@ fun RencanaStudyView(
                 contentDescription = ""
             )
             Spacer(modifier = Modifier.padding(start = 30.dp))
+
+            Column (modifier = Modifier.weight(1f)){
+                Text(
+                    text = mahasiswa.nama,
+                    color = Color.White,
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold
+                )
+
+                Text()
+            }
         }
     }
 }
